d44d9ed546232a759add792a3df060cec32fd765
iFoxxy <ifoxxy777@gmail.com>
2016-02-18 20:26:52 +0300

    Project created

---
+++ RCG Personnel/RCG Personnel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RCG Personnel
+//
+//  Created by iFoxxy on 18.02.16.
+//  Copyright © 2016 LightBlueFox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ RCG Personnel/RCG PersonnelTests/RCG_PersonnelTests.swift
@@ -0,0 +1,36 @@
+//
+//  RCG_PersonnelTests.swift
+//  RCG PersonnelTests
+//
+//  Created by iFoxxy on 18.02.16.
+//  Copyright © 2016 LightBlueFox. All rights reserved.
+//
+
+import XCTest
+@testable import RCG_Personnel
+
+class RCG_PersonnelTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
