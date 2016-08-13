99b67db44b4662c8cf57d641f173588c8760b7ce
HiveHicks <hivehicks@gmail.com>
2016-08-13 18:33:02 +0300

    Extract AuthenticationServiceImpl to separate file. Move recreation of root view controller on sign out to app delegate with a help of notifications.

--- RCG Personnel/RCG Personnel/AppDelegate.swift
+++ RCG Personnel/RCG Personnel/AppDelegate.swift
@@ -40,15 +40,14 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         //Временная папочка с дефаултсами
         print(NSTemporaryDirectory())
         
-        NSNotificationCenter.defaultCenter().addObserver(self, selector:#selector(AppDelegate.checkForReachability(_:)), name: ReachabilityChangedNotification, object: nil);
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        
+        notificationCenter.addObserver(self, selector:#selector(AppDelegate.checkForReachability(_:)), name: ReachabilityChangedNotification, object: nil);
         
         do {
-            try
-            self.reachability = Reachability.reachabilityForInternetConnection();
-            try
-            self.reachability.startNotifier()
-        }
-        catch {
+            self.reachability = try Reachability.reachabilityForInternetConnection()
+            try self.reachability.startNotifier()
+        } catch {
             print(error)
         }
         
@@ -76,7 +75,9 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
             self.window?.rootViewController?.presentViewController(rootViewController, animated: true, completion: nil)
         }*/
         
-        
+        notificationCenter.addObserverForName(userDidSignOutNotification, object: nil, queue: NSOperationQueue.mainQueue()) { [weak self] notification in
+            self?.recreateRootViewController()
+        }
         
         return true
     }
@@ -283,29 +284,9 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         })
     }
     
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    private func recreateRootViewController() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        window?.rootViewController = storyboard.instantiateInitialViewController()
     }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-            FBSDKAppEvents.activateApp()
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
 
